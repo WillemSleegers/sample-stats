f84a91580a843f0b071988992203f2c80e27e0ef
Willem Sleegers <w.sleegers@me.com>
2025-01-28 09:20:48 +0100

    Added react scan

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script src="https://unpkg.com/react-scan/dist/auto.global.js" async />
+      </head>
       <body className="antialiased">
         <ThemeProvider
           attribute="class"
